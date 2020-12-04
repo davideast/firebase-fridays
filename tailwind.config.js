a735f110af72c3e30b085bb2df304ecab6825aea
David East <deast@google.com>
2020-12-04 11:15:40 -0500

    make it so

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [
+    './public/**/*.html',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      'body': ["'Roboto'", 'system-ui', 'Arial', 'sans-serif'],
+      'display': ["'Rubik'", 'system-ui', 'Arial', 'sans-serif'],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
